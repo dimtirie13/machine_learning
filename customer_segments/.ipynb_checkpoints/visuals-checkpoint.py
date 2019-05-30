3594c49e3079604d0f0e8563506a59a828ccad6e
dimitrie13 <dimitrie13@outlook.com>
2019-05-30 11:18:13 -0400

    project completed

--- customer_segments/.ipynb_checkpoints/visuals-checkpoint.py
+++ customer_segments/.ipynb_checkpoints/visuals-checkpoint.py
@@ -129,11 +129,11 @@ def channel_results(reduced_data, outliers, pca_samples):
 	'''
 
 	# Check that the dataset is loadable
-	try:
-	    full_data = pd.read_csv("customers.csv")
-	except:
-	    print("Dataset could not be loaded. Is the file missing?")       
-	    return False
+# 	try:
+	full_data = pd.read_csv("customers.csv")
+# 	except:
+# 	    print("Dataset could not be loaded. Is the file missing?")       
+# 	    return False
 
 	# Create the Channel DataFrame
 	channel = pd.DataFrame(full_data['Channel'], columns = ['Channel'])
